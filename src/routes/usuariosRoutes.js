cd97e57d7b690196ddc0a100e0be5184bce85eb0
Matias <matiasezequielsmania@gmail.com>
2024-11-07 17:24:46 -0300

    contraseña

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const usuariosControllers = require('../controllers/usuariosControllers');
+const { verificarToken, tokenAdmin } = require('../middlewares/authMiddleware');
 
-// Ruta para iniciar sesion
+// Ruta para iniciar sesión
 router.post("/iniciarSesion", usuariosControllers.iniciarSesion);
 
-// Ruta para crear un nuevo usuario
+// Ruta para crear un nuevo usuario (solo admin puede hacerlo)
 router.post("/crearUsuario", usuariosControllers.crearUsuario);
 
-// Ruta para listar todos los usuarios
-router.get("/obtenerUsuarios", usuariosControllers.obtenerUsuarios);
+// Ruta para listar todos los usuarios (solo admin)
+router.get("/obtenerUsuarios", verificarToken, tokenAdmin, usuariosControllers.obtenerUsuarios);
 
-// Ruta para obtener un usuario por ID
-router.get("/obtenerUsuario/:id", usuariosControllers.obtenerUsuarioPorId);
+// Ruta para obtener un usuario por ID (solo admin puede hacerlo)
+router.get("/obtenerUsuario/:id", verificarToken, tokenAdmin, usuariosControllers.obtenerUsuarioPorId);
 
-// Ruta para actualizar un usuario por ID
-router.put("/actualizarUsuario/:id", usuariosControllers.actualizarUsuario);
+// Ruta para actualizar un usuario por ID (solo admin puede hacerlo)
+router.put("/actualizarUsuario/:id", verificarToken, tokenAdmin, usuariosControllers.actualizarUsuario);
 
-// Ruta para eliminar un usuario por ID
-router.delete("/eliminarUsuario/:id", usuariosControllers.eliminarUsuario);
+// Ruta para eliminar un usuario por ID (solo admin puede hacerlo)
+router.delete("/eliminarUsuario/:id", verificarToken, tokenAdmin, usuariosControllers.eliminarUsuario);
 
 module.exports = router;
